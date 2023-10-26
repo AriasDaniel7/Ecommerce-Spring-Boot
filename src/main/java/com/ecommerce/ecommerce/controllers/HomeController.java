75a9385e7055c3929e21b7db406461a24580650e
Daniel Arias <daniel7arias2003@gmail.com>
2023-10-26 14:22:10 -0500

    Mostrar los productos en la vista carrito

--- src/main/java/com/ecommerce/ecommerce/controllers/HomeController.java
+++ src/main/java/com/ecommerce/ecommerce/controllers/HomeController.java
@@ -1,5 +1,10 @@
 package com.ecommerce.ecommerce.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,7 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ecommerce.ecommerce.models.DetalleOrden;
+import com.ecommerce.ecommerce.models.Orden;
 import com.ecommerce.ecommerce.models.Producto;
 import com.ecommerce.ecommerce.services.ProductoService;
 
@@ -15,10 +23,16 @@ import com.ecommerce.ecommerce.services.ProductoService;
 @RequestMapping("/")
 public class HomeController {
 
-    // private final Logger log = LoggerFactory.getLogger(HomeController.class);
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @Autowired
     private ProductoService serviceProducto;
+
+    // Para alamacenar los detalles de la orden
+    List<DetalleOrden> detalles = new ArrayList<>();
+
+    // Almacenar los datos de la orden
+    Orden orden = new Orden();
 
     @GetMapping("")
     public String mostrarHome(Model model) {
@@ -35,7 +49,25 @@ public class HomeController {
     }
 
     @PostMapping("carrito")
-    public String agregarCarrito() {
+    public String agregarCarrito(@RequestParam Integer id, @RequestParam Integer cantidad, Model model) {
+
+        Producto producto = serviceProducto.obtenerPorId(id).get();
+        double sumaTotal = 0;
+        DetalleOrden detalleOrden = new DetalleOrden();
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
+        detalles.add(detalleOrden);
+
+        sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+        orden.setTotal(sumaTotal);
+        model.addAttribute("carrito", detalles);
+        model.addAttribute("orden", orden);
+        // log.info("Producto añadido: {}", producto);
+        // log.info("Cantidad: {}", cantidad);
         return "usuario/carrito";
     }
 }
--- src/main/java/com/ecommerce/ecommerce/models/DetalleOrden.java
+++ src/main/java/com/ecommerce/ecommerce/models/DetalleOrden.java
@@ -11,6 +11,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "detalles")
 public class DetalleOrden {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -28,12 +29,14 @@ public class DetalleOrden {
     public DetalleOrden() {
     }
 
-    public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
+    public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total, Orden orden, Producto producto) {
         this.id = id;
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.precio = precio;
         this.total = total;
+        this.orden = orden;
+        this.producto = producto;
     }
 
     public Integer getId() {
